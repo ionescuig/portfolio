8795e02c68d654d768c4786d96e01eba60d9591c
Gabriel <ionescuig@yahoo.com>
2019-02-28 19:58:05 +0000

    fixed no update problem in project update

--- src/projects/forms.py
+++ src/projects/forms.py
@@ -13,11 +13,12 @@ class ProjectForm(forms.ModelForm):
         }
 
     def __init__(self, *args, **kwargs):
-        # make a list of available positions for the new project
+        # make a list of available positions
         total_projects = Project.objects.all().count()
         position_list = [x for x in range(1, total_projects+2)]
         titles_list = list(Project.objects.all().values_list('title', flat=True).order_by('position'))
 
+        # the form position will a ChoiceField
         choices_titles_list = []
         for elem in titles_list:
             choices_titles_list.append('%s: %s' % (Project.objects.values_list('position', flat=True).get(title=elem),
@@ -26,10 +27,14 @@ class ProjectForm(forms.ModelForm):
 
         choices = map(lambda x, y: (x, y), position_list, choices_titles_list)
 
-        # update the position field with the new list
+        # offer the choice to pick from list of projects
+        # for new project, initial position will be the no of project + 1
         super(ProjectForm, self).__init__(*args, **kwargs)
         self.fields['position'] = forms.ChoiceField(choices=choices, required=True)
-        self.initial['position'] = total_projects + 1
+
+        # check if existing or new project
+        if not self.instance.position:
+            self.initial['position'] = total_projects + 1
 
 
 class ImagesForm(forms.ModelForm):
